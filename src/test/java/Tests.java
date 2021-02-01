edd61889c4050c94fc93bc9c6aa63180ef10a8d1
Anyela92 <luz.1701011041@ucaldas.edu.co>
2021-02-01 12:44:14 -0500

    CP Buscar y crear cuenta

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ByIdOrName;
 
-public class TestBuscar {
+public class Tests {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver","C:\\chromeDriver\\chromedriver.exe");
@@ -13,6 +13,19 @@ public class TestBuscar {
         By locatoR = new ByIdOrName("search_query_top");
         WebElement searchBox= driver.findElement(locatoR);
         searchBox.sendKeys("Dress");
+        By locator = By.className("login");
+        WebElement element = driver.findElement(locator);
+        element.click();
+
+        driver.findElement(By.id("email_create")).sendKeys("testing.handelbay@gmail.com");
+        driver.findElement(By.id("SubmitCreate")).click();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        String pageHeading = driver.findElement(By.className("page-heading")).getText();
+        System.out.println("Page Heading: "+pageHeading);
 
     }
 }
